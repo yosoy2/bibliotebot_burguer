9335813f1721b0134940dbfe60bf171363534361
yosoy2 <edotercero66.el@gmail.com>
2021-05-28 15:33:36 -0400

    bibliotebot pkg

---
+++ build/bibliotebot_vision/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/edu/bibliotebot_burguer/src/bibliotebot_vision
+# Build directory: /home/edu/bibliotebot_burguer/build/bibliotebot_vision
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
